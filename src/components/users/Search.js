eb269e8b88e8b06187fa26d9f4f4382081c5d24f
Rayne Scofield <raynescofield@gmail.com>
2021-05-22 19:49:09 +0800

    first react app

---
+++ src/components/users/Search.js
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from "react";
+import GithubContext from "../../context/github/githubcontext";
+import AlertContext from "../../context/alert/alertContext";
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const [text, setText] = useState("");
+  const { setAlert } = alertContext;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      setAlert("please enter something", "light");
+    } else {
+      githubContext.searchUsers(text);
+      setText("");
+    }
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
